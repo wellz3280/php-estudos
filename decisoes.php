9eb76cc4b1dd15b34a1dedf74141126fe045179e
welington <welington.a2you@gmail.com>
2021-09-22 16:05:11 -0300

     exemplos de estruturas de decisão if e else, ane e or

--- decisoes.php
+++ decisoes.php
@@ -1,3 +1,12 @@
 <?php
 
+	$idade = 25;
 	
+
+	echo "Você só pode entrar se tiver mais do que 18 anos.".PHP_EOL;
+
+	if($idade >= 18 ){
+		echo "Você tem $idade anos. Pode entrar".PHP_EOL;
+	}else{
+		echo "Você tem apenas $idade anos. Não pode entrar".PHP_EOL;
+	}
