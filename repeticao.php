e918403c76a3026c85edfc06ec85f93110cc6139
welington <welington.a2you@gmail.com>
2021-09-22 16:52:09 -0300

     Loop while e for

---
+++ repeticao.php
@@ -0,0 +1,17 @@
+<?php
+	$contator = 1;
+	/*
+		Laço enquanto (while)
+
+	while($contador <= 15){
+		echo "#".$contador.PHP_EOL;
+		$contador += +1;
+	}
+	*/
+
+	//Laço Para (for)
+
+	for($contador = 1;$contador <= 15;$contador++)
+	{
+		echo "#".$contador.PHP_EOL;
+	}
