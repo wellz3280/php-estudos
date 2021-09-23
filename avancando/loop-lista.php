9872e0e7cb01b6e9f34a00150847eb7ee7797b2b
welington <welington.a2you@gmail.com>
2021-09-23 18:02:39 -0300

    criada lista de nomes

--- avancando/loop-lista.php
+++ avancando/loop-lista.php
@@ -1,14 +1,15 @@
 <?php
 
 	$idadeList = [21,23,22,15,42,32,33];
-	
-	for($i = 0; $i < count($idadeList);$i++){
+	$nomeList = array("joão","josé","marcos","camila","sergio","karla","leticia");
 
-		echo $idadeList[$i]. PHP_EOL;
+	for($i = 0; $i < count($nomeList);$i++){
+
+		echo $nomeList[$i]. PHP_EOL;
 	}
 	
 	 
-	  
+
 	/*
 	$cont = 0;
 
