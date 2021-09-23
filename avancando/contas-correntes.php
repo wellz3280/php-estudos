71053e4ce3db9fa2869161caf975126d27a4eedc
welington <welington.a2you@gmail.com>
2021-09-23 18:26:19 -0300

    estudando array associativos

--- avancando/contas-correntes.php
+++ avancando/contas-correntes.php
@@ -1,3 +1,29 @@
 <?php
+	//array associativos
 
+	$conta1 =[
+
+		'titular' => 'weliton',
+		'saldo' => 4000
+	]; 
+
+	$conta2 = [
+		'titular' => 'jean',
+		'saldo' => 10000
+	];
+
+	$conta3 = [
+		'titular' => 'karla',
+		'saldo' => 320
+	];
+
+
+
+	$contasCorrentes = [$conta1,$conta2,$conta3];
+
+	
+	
+	for ($i=0; $i < count($contasCorrentes) ; $i++) { 
+		echo $contasCorrentes[$i]['titular']. PHP_EOL;
+	}
 	
