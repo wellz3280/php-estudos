7c4f8d74acaeb9fdc1721f69a9ab589f23b1ae0d
welington <welington.a2you@gmail.com>
2021-09-23 17:59:20 -0300

    diretorio criado

---
+++ avancando/listas.php
@@ -0,0 +1,7 @@
+<?php
+		
+		$idadeList = [21,23,22,15,42,32];
+
+		$umaIdade = $idadeList[5];
+
+		echo $umaIdade;
---
+++ avancando/loop-lista.php
@@ -0,0 +1,20 @@
+<?php
+
+	$idadeList = [21,23,22,15,42,32,33];
+	
+	for($i = 0; $i < count($idadeList);$i++){
+
+		echo $idadeList[$i]. PHP_EOL;
+	}
+	
+	 
+	  
+	/*
+	$cont = 0;
+
+	while ($cont < count($idadeList)) {
+	
+	echo $idadeList[$cont]. PHP_EOL;
+		$cont += 1;
+	}
+	*/
