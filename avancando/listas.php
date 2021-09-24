744558cca7e391489f74fbe2631a9e546ad1808e
welington <welington.a2you@gmail.com>
2021-09-24 18:33:33 -0300

    adiconadondo no array mais um item

--- avancando/listas.php
+++ avancando/listas.php
@@ -2,6 +2,11 @@
 		
 		$idadeList = [21,23,22,15,42,32];
 
-		$umaIdade = $idadeList[5];
+		$idadeList[] = 20;
+		
+		foreach ($idadeList as $idade) {
+				
+				echo $idade. PHP_EOL;
+		}
 
-		echo $umaIdade;
+		
