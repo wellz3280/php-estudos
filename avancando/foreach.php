6603eba54c1ead40e3917d6800d4f124f0de9225
welington <welington.a2you@gmail.com>
2021-09-24 18:34:08 -0300

    avançando com os estudos de foreach

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -16,8 +16,15 @@
 		['titular' => 'karla',
 		'saldo' => 320]
 	];
+	//adicionando conta a lista
+	$contasCorrentes[26429875812] = [
+		'titular' => 'claudia',
+		'saldo' => 20000
 
+	];
+
+	//foreach (para cada um)
 foreach($contasCorrentes as $cpf => $contas){
-	echo $cpf. PHP_EOL;
+	echo $cpf." ".$contas['titular']. PHP_EOL;
 }	
 	
