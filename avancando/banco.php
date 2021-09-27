a943f937988816f85985f841cbea673232a1adb1
welington <welington.a2you@gmail.com>
2021-09-27 20:34:21 -0300

    desenvolvendo a função sacar e depositar

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,7 +1,7 @@
 <?php
 	//array associativos e foreach
 	
-	function sacar($conta ,$valorAsacar)
+	function sacar( array $conta ,float $valorAsacar): array
 	{
 			if($valorAsacar > $conta['saldo'])
 		{
@@ -14,8 +14,23 @@
 		return $conta;
 	}
 	
+	function depositar(array $conta,float $valorAdepositar):array
+	{
 
-	function exibeMensagem($mensagem)
+		if($valorAdepositar > 0){
+
+		$conta['saldo'] += $valorAdepositar;
+		}
+		else{
+			exibeMensagem("Não é possivel realizar este depósito.");
+		}
+
+		return $conta;
+	}
+	
+
+
+	function exibeMensagem(string $mensagem)
 	{
 		echo $mensagem . PHP_EOL;
 	}
@@ -36,11 +51,18 @@
 		'saldo' => 320]
 	];
 
+	$contasCorrentes [12767278912]= [
+			'titular' => 'cleide',
+			'saldo' => 120000
+		];
+
 	$contasCorrentes[16339472903] = sacar($contasCorrentes[16339472903],500);
+	
+	$contasCorrentes[36529972803] = depositar($contasCorrentes[36529972803],3500);
 
 	
 	//foreach (para cada um)
 foreach($contasCorrentes as $cpf => $contas){
-	exibeMensagem( $cpf." ".$contas['titular']." ".$contas['saldo']);
+	exibeMensagem( "$cpf $contas[titular] $contas[saldo]");
 }	
 	
